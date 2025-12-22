6ee610c6097595d6e399e7a24b25d9d974e6034d
Sabrina Bekirova <sabrina.bekirova@baruchmail.cuny.edu>
2025-12-22 17:00:36 -0500

    Remove .aws-sam/build

--- .aws-sam/build/ExpressFunction/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-var router = Router();
-
-router.get('/api/test-get', function(req, res, next) {
-  res.json({ message: 'Hello from the API!' });
-});
-
-export default router;
